67f6a6b34069cfabd8917da00fe2a3365e7a9c7b
Donny <donny.redmond@gmail.com>
2014-07-18 11:37:31 -0500

    Added jsExtenderExamples.js and examples.html

---
+++ jsExtenderExamples.js
@@ -0,0 +1,13 @@
+(function() {
+	var output = document.getElementById('output');
+	
+	function writeOutput(msg) {
+		output.innerHTML += msg + '<br />';
+	}
+	
+	/*writeOutput('testing');
+	writeOutput('testing');
+	writeOutput('testing');
+	writeOutput('testing');
+	writeOutput('testing');*/
+})();
