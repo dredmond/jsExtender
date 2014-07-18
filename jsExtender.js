249feff15df01f97abd214d01de04c703fb69d9d
Donny <donny.redmond@gmail.com>
2014-07-18 16:07:04 -0500

    Finished writing and testing examples.

--- jsExtender.js
+++ jsExtender.js
@@ -1,109 +1,119 @@
-﻿var jsExtender = jsExtender || (function (classExtension) {
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        getOwnPropertyNames = Object.getOwnPropertyNames;
+﻿var jsExtender = jsExtender || (function () {
+	var hasOwnProperty = Object.prototype.hasOwnProperty,
+		getOwnPropertyNames = Object.getOwnPropertyNames,
+		keys = Object.keys;
+		
+	var _jsExtender = function (classExtension) {
+		// Takes object or prototype and makes a new constructor function
+		// that inherits it.
+		function createProto(e) {
+			function func() { }
+			func.prototype = e;
+			return new func();
+		}
 
-    // Takes object or prototype and makes a new constructor function
-    // that inherits it.
-    function createProto(e) {
-        function func() { }
-        func.prototype = e;
-        return new func();
-    }
+		function isObjectConstructor(func) {
+			return (Object.prototype.constructor === func);
+		}
 
-    function isObjectConstructor(func) {
-        return (Object.prototype.constructor === func);
-    }
+		function getPrototypeObject(extender) {
+			return (typeof (extender) === 'object') ? extender : extender.prototype;
+		}
 
-    function getPrototypeObject(extender) {
-        return (typeof (extender) === 'object') ? extender : extender.prototype;
-    }
+		// Copys all properties from source to the destination if they don't already
+		// exist in the destination. Otherwise, it creates a new function that wraps around
+		// the source function and allows the inherited function to call the base function (destination).
+		function copyProperties(destination, source) {
+			var propNames = getOwnPropertyNames(source);
 
-    // Copys all properties from source to the destination if they don't already
-    // exist in the destination. Otherwise, it creates a new function that wraps around
-    // the source function and allows the inherited function to call the base function (destination).
-    function copyProperties(destination, source) {
-        var propNames = getOwnPropertyNames(source);
+			for (var i = 0; i < propNames.length; i++) {
+				var p = propNames[i];
 
-        for (var i = 0; i < propNames.length; i++) {
-            var p = propNames[i];
+				if (typeof (destination[p]) === 'function' && typeof (source[p]) === 'function') {
+					destination[p] = wrapFunction(destination[p], source[p]);
+				} else {
+					destination[p] = source[p];
+				}
+			}
+		}
 
-            if (typeof (destination[p]) === 'function' && typeof (source[p]) === 'function') {
-                destination[p] = wrapFunction(destination[p], source[p]);
-            } else {
-                destination[p] = source[p];
-            }
-        }
-    }
+		/* 
+		 * Function wrapper for existing destination functions.
+		 * Creates and returns a new function that copies the old base function
+		 * and replaces it with the new base function. 
+		 * 
+		 * It calls the source function with the current arguments and stores the result.
+		 * Then it replaces the base function with the old base function and returns the
+		 * result of the source function.
+		 */
+		function wrapFunction(baseFunc, sourceFunc) {
+			return function () {
+				var oldBase = this.base;
 
-    /* 
-     * Function wrapper for existing destination functions.
-     * Creates and returns a new function that copies the old base function
-     * and replaces it with the new base function. 
-     * 
-     * It calls the source function with the current arguments and stores the result.
-     * Then it replaces the base function with the old base function and returns the
-     * result of the source function.
-     */
-    function wrapFunction(baseFunc, sourceFunc) {
-        return function () {
-            var oldBase = this.base;
+				this.base = baseFunc;
+				var result = sourceFunc.apply(this, arguments);
+				this.base = oldBase;
 
-            this.base = baseFunc;
-            var result = sourceFunc.apply(this, arguments);
-            this.base = oldBase;
+				return result;
+			};
+		}
 
-            return result;
-        };
-    }
+		/*
+		 * Creates a new extend function and adds it to the destination.
+		 * This allows the destination to stay in scope for the next extend call.
+		 */
+		function addExtend(destination) {
+			destination.extend = function (extender) {
+				var currentExtender = getPrototypeObject(extender),
+					extendProto = createConstructor(currentExtender, destination);
 
-    /*
-     * Creates a new extend function and adds it to the destination.
-     * This allows the destination to stay in scope for the next extend call.
-     */
-    function addExtend(destination) {
-        destination.extend = function (extender) {
-            var currentExtender = getPrototypeObject(extender),
-                extendProto = createConstructor(currentExtender, destination);
+				extendProto.prototype = createProto(destination.prototype);
+				copyProperties(extendProto.prototype, currentExtender);
 
-            extendProto.prototype = createProto(destination.prototype);
-            copyProperties(extendProto.prototype, currentExtender);
+				addExtend(extendProto);
 
-            addExtend(extendProto);
+				return extendProto;
+			};
+		}
 
-            return extendProto;
-        }
-    }
+		function createConstructor(source, destination) {
+			function defaultConstructor() { }
 
-    function createConstructor(source, destination) {
-        function defaultConstructor() { }
+			if (!source || typeof (source.constructor) !== 'function') {
+				return defaultConstructor;
+			}
 
-        if (!source || typeof (source.constructor) !== 'function') {
-            return defaultConstructor;
-        }
+			var proto = source.constructor,
+				invalidConstructor = isObjectConstructor(proto);
 
-        var proto = source.constructor,
-            invalidConstructor = isObjectConstructor(proto);
+			if (!destination || typeof (destination.prototype.constructor) !== 'function') {
+				return (!invalidConstructor) ? proto : defaultConstructor;
+			}
 
-        if (!destination || typeof (destination.prototype.constructor) !== 'function') {
-            return (!invalidConstructor) ? proto : defaultConstructor;
-        }
+			if (!invalidConstructor) {
+				return wrapFunction(destination.prototype.constructor, proto);
+			}
 
-        if (!invalidConstructor) {
-            return wrapFunction(destination.prototype.constructor, proto);
-        }
+			return destination.prototype.constructor;
+		}
 
-        return destination.prototype.constructor;
-    }
+		if (!classExtension) {
+			classExtension = {};
+		}
 
-    if (!classExtension) {
-        classExtension = {};
-    }
+		var baseExtend = getPrototypeObject(classExtension),
+			classConstruct = createConstructor(baseExtend);
 
-    var baseExtend = getPrototypeObject(classExtension),
-        classConstruct = createConstructor(baseExtend);
+		classConstruct.prototype = createProto(baseExtend);
+		addExtend(classConstruct);
 
-    classConstruct.prototype = createProto(baseExtend);
-    addExtend(classConstruct);
+		return classConstruct;
+	};
 
-    return classConstruct;
-});
+	_jsExtender.hasOwnProperty = hasOwnProperty;
+	_jsExtender.getOwnPropertyNames = getOwnPropertyNames;
+	_jsExtender.keys = keys;
+	
+	return _jsExtender;
+})();
+ 
--- jsExtenderExamples.js
+++ jsExtenderExamples.js
@@ -1,13 +1,200 @@
-(function() {
-	var output = document.getElementById('output');
+var jsExtExamples = (function() {
+	// Private Variables
+	var output = null;
 	
-	function writeOutput(msg) {
-		output.innerHTML += msg + '<br />';
+	function applyIndent(msg, indent) {
+		var i = 0,
+			spaces = '';
+		while (i++ < indent) {
+			spaces += '&nbsp;';
+		}
+		
+		return spaces + msg;
 	}
 	
-	/*writeOutput('testing');
-	writeOutput('testing');
-	writeOutput('testing');
-	writeOutput('testing');
-	writeOutput('testing');*/
+	// extenderExamplesClass Definition
+	var extenderExamplesClass = jsExtender({
+			constructor: function () {
+				output = document.getElementById('output');
+			},
+			writeOutput: function (msg, indent) {
+				if (output === null)
+					return;
+				
+				msg = typeof(msg) !== 'undefined' ? msg : '';
+				
+				if (typeof(indent) === 'undefined' || indent == null)
+					indent = 0;
+				
+				if (Object.prototype.toString.call(msg) === '[object Array]') {
+					var objMsg = '';
+					for (var i = 0; i < msg.length; i++) {
+						var value = msg[i],
+							propType = typeof (msg[i]);
+						objMsg += applyIndent('[' + i + '] = ' + value + ' (' + propType + ')<br />', indent);
+					}
+					msg = objMsg;
+				} else if (typeof (msg) === 'object') {
+					var objMsg = '';
+					for (var propName in msg) {
+						var propType = typeof (msg[propName]);
+						objMsg += applyIndent(propName + ' (' + propType + ')<br />', indent);
+					}
+					msg = objMsg;
+				} else {
+					msg = applyIndent(msg + '<br />', indent);
+				}
+				
+				output.innerHTML += msg;
+			}
+		});
+	
+	// Create instance and return it.
+	var extenderExamples = new extenderExamplesClass();
+	return extenderExamples;
 })();
+
+jsExtExamples.writeOutput('jsExtExamples Properties:');
+jsExtExamples.writeOutput(jsExtExamples, 4);
+jsExtExamples.writeOutput();
+
+// Default jsExtender
+var defaultClass = jsExtender(),
+	defaultInstance = new defaultClass();
+
+jsExtExamples.writeOutput('defaultClass Properties:');
+jsExtExamples.writeOutput(jsExtender.keys(defaultClass), 4);
+jsExtExamples.writeOutput();
+
+// jsExtender using Object Definition
+var objectDefinedClass = jsExtender({
+	constructor: function (arg1, arg2) {
+		this.value1 = arg1;
+		this.value2 = arg2;
+	},
+	publicVar: 'This is a public variable.',
+	addValues: function (x, y, z) {
+		return x + y + z;
+	}
+});
+
+jsExtExamples.writeOutput('objectDefinedClass Properties:');
+jsExtExamples.writeOutput(jsExtender.keys(objectDefinedClass), 4);
+jsExtExamples.writeOutput();
+
+var objectDefinedInstance = new objectDefinedClass('something1', 2);
+jsExtExamples.writeOutput('objectDefinedInstance Properties:');
+jsExtExamples.writeOutput(objectDefinedInstance, 4);
+jsExtExamples.writeOutput();
+
+jsExtExamples.writeOutput('objectDefinedInstance.addValues: ' + objectDefinedInstance.addValues(10, 2, 1));
+jsExtExamples.writeOutput(objectDefinedInstance.value1);
+jsExtExamples.writeOutput(objectDefinedInstance.value2);
+jsExtExamples.writeOutput(objectDefinedInstance.publicVar);
+jsExtExamples.writeOutput();
+
+// Using jsExtender's default constructor.
+var defaultClass = jsExtender();
+
+defaultClass.prototype.testFunc = function () {
+    console.log('Testing the test function!');
+};
+
+// Create a defaultClass object.
+var defaultInstance = new defaultClass();
+defaultInstance.testFunc();
+
+// Extend the default class and override the constructor.
+var defaultClassExtended = defaultClass.extend({
+    constructor: function () {
+        // Override default constructor.
+        console.log('The default constructor has been overridden!');
+    }
+});
+
+// Create a new instance of the extended default class.
+defaultInstanceExtended = new defaultClassExtended();
+
+// The testFunc declared on defaultClass should still work in defaultClassExtended.
+defaultInstanceExtended.testFunc();
+
+// The constructor function that will inherit from defaultClass.
+function ExtensionConstructor () {
+    console.log('The default constructor has been overridden by the ExtensionConstructor!');
+}
+
+// This prototyped function will be available after the inheritance occurs.
+ExtensionConstructor.prototype.myPrototypedFunction = function (x, y) {
+    console.log('x: ' + x);
+    console.log('y: ' + y);
+};
+
+// Make ExtensionConstructor inherit from the default class.
+var extensionFunctionClass = defaultClass.extend(ExtensionConstructor);
+
+// Create a new instance of the extended default class.
+var extensionFunctionInstance = new extensionFunctionClass();
+
+// The function declared on the ExtensionConstructor should still work.
+extensionFunctionInstance.myPrototypedFunction(20, 50);
+
+// The testFunc declared on defaultClass should still work in extensionFunctionInstance.
+extensionFunctionInstance.testFunc();
+
+var baseTestClass = extensionFunctionClass.extend({
+    constructor: function() {
+        console.log('Constructor from baseTestClass has been called.');
+        this.base.call(this);
+    },
+    testFunc: function() {
+        console.log('The test function has been called from the baseTestClass.');
+        this.base.call(this);
+    },
+    testFunc2: function() {
+        console.log('Testing testFunc2 from baseTestClass.');
+    },
+    myPrototypedFunction: function(x, y, z) {
+        this.base.call(this, x, y);
+        console.log('z: ' + z);
+    }
+});
+
+// Make an instace of the baseTestClass. It should call 
+// it's own constructor and the constructor of it's parent.
+var baseTestInstance = new baseTestClass();
+
+// Make sure all other calls still work as expected. 
+// They should also call the base functions.
+baseTestInstance.testFunc();
+baseTestInstance.testFunc2();
+baseTestInstance.myPrototypedFunction(10, 20, 30);
+
+// Multiple Inheritance with base calls.
+var baseTestClass2 = baseTestClass.extend({
+    constructor: function() {
+        console.log('Constructor from baseTestClass2 has been called.');
+        this.base.call(this);
+    },
+    testFunc: function() {
+        console.log('The test function has been called from the baseTestClass2.');
+        this.base.call(this);
+    },
+    testFunc2: function() {
+        this.base.call(this);
+        console.log('Testing testFunc2 from baseTestClass2.');
+    },
+    myPrototypedFunction: function(x, y, z) {
+        console.log('myPrototypedFunction has been called from baseTestClass2.');
+        this.base.call(this, x, y, z);
+    }
+});
+
+// Make an instace of the baseTestClass2. It should call 
+// it's own constructor and the constructor of it's parent.
+var baseTestInstance2 = new baseTestClass2();
+
+// Make sure all other calls still work as expected. 
+// They should also call the base functions.
+baseTestInstance2.testFunc();
+baseTestInstance2.testFunc2();
+baseTestInstance2.myPrototypedFunction(10, 10, 10);
